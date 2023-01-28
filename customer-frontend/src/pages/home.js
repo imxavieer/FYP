5e1a678eeae9624b9c66bbdb23d0d94d6a36f0b9
lokim0215 <92701955+lokim0215@users.noreply.github.com>
2023-01-28 14:59:21 +0800

    Footer component separated

--- customer-frontend/src/pages/home.js
+++ customer-frontend/src/pages/home.js
@@ -1,64 +1,7 @@
 import React from "react";
-import Entrance from "../honeynightentrance.jpg";
-import Box from "@mui/material/Box";
-import { styled } from "@mui/system";
-import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import Footer from "../components/Footer";
+
 import "./home.css";
-function SocialMedia({ classes }) {
-  return (
-    // From 0 to 600px wide (smart-phones), I take up 12 columns, or the whole device width!
-    // From 600-690px wide (tablets), I take up 6 out of 12 columns, so 2 columns fit the screen.
-    // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
-    <Grid item xs={12} sm={6} md={4}>
-      <div className="center-text">
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="label"
-          onClick={() => window.open('https://www.instagram.com/honeynightsg/')}
-        >
-          <InstagramIcon color="warning" fontSize="large"/>
-        </IconButton>
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="label"
-          onClick={() => window.open('https://www.facebook.com/HONEYNIGHTSG/')}
-        >
-          <FacebookIcon color="warning" fontSize="large"/>
-        </IconButton>
-      </div>
-    </Grid>
-  );
-}
-function CompanyLogo({ classes }) {
-  return (
-    // From 0 to 600px wide (smart-phones), I take up 12 columns, or the whole device width!
-    // From 600-690px wide (tablets), I take up 6 out of 12 columns, so 2 columns fit the screen.
-    // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
-    <Grid item xs={12} sm={6} md={4}>
-      <div className="center-text company-logo">
-        <img
-          src="https://honeytesting.netlify.app/assets/images/logo/logo_bottom_yellow.png"
-          alt="honeynight logo"
-        />
-      </div>
-    </Grid>
-  );
-}
-function CopyRight({ classes }) {
-  return (
-    // From 0 to 600px wide (smart-phones), I take up 12 columns, or the whole device width!
-    // From 600-690px wide (tablets), I take up 6 out of 12 columns, so 2 columns fit the screen.
-    // From 960px wide and above, I take up 25% of the device (3/12), so 4 columns fit the screen.
-    <Grid item xs={12} sm={12} md={4}>
-      <div className="center-text copyright">© Copyright Honey Night Co.</div>
-    </Grid>
-  );
-}
 
 function Home() {
   return (
@@ -83,13 +26,7 @@ function Home() {
           }}
         />
       </div>
-      <div className="footer-bg">
-        <Grid container spacing={1}>
-          <SocialMedia />
-          <CompanyLogo />
-          <CopyRight />
-        </Grid>
-      </div>
+      <Footer></Footer>
     </>
   );
 }
