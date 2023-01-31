1449cd3c721895ac7aab6e21e5c4e97f64e35b14
Setsuna Yukiii <siang-meng@live.com>
2023-01-31 11:35:59 +0800

    add menu pdf button

--- customer-frontend/src/pages/menu.js
+++ customer-frontend/src/pages/menu.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Button, Link } from "@mui/material";
 import MenuItem from "../components/menu/MenuItem";
+import MenuPdf from "../documents/menu.pdf";
 import "./menu.css";
 function Menu() {
     // MenuItem
@@ -10,42 +11,48 @@ function Menu() {
     // menuItemDescription --> description of the menu item
     const menuItems = [
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 1",
             menuItemPrice: "$25",
             menuItemDescription:
                 "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate",
         },
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 2",
             menuItemPrice: "$25",
             menuItemDescription:
                 "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate",
         },
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 3",
             menuItemPrice: "$25",
             menuItemDescription:
                 "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate",
         },
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 4",
             menuItemPrice: "$25",
             menuItemDescription:
                 "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate",
         },
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 5",
             menuItemPrice: "$25",
             menuItemDescription:
                 "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate",
         },
         {
-            menuImage: "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
+            menuImage:
+                "https://res.cloudinary.com/duxqahhdk/image/upload/v1675128915/samples/food/pot-mussels.jpg",
             menuItemName: "Menu Item 6",
             menuItemPrice: "$25",
             menuItemDescription:
@@ -73,7 +80,37 @@ function Menu() {
                         />
                     );
                 })}
+                {/* <Grid item xs={12}>
+                    <Link target={"_blank"} href={MenuPdf}>
+                        <Button
+                            variant="filled"
+                            className={"actionButton"}
+                            sx={{
+                                background: "#e19200",
+                                color: "white",
+                                margin: "10px auto",
+                            }}
+                        >
+                            View Full Menu
+                        </Button>
+                    </Link>
+                </Grid> */}
             </Grid>
+            <Link target={"_blank"} href={MenuPdf}>
+                <Button
+                    variant="filled"
+                    className={"actionButton"}
+                    sx={{
+                        background: "#e19200",
+                        color: "white",
+                        margin: "10px auto",
+                        display:"flex",
+                        padding:"10px"
+                    }}
+                >
+                    View Full Menu
+                </Button>
+            </Link>
         </div>
     );
 }
