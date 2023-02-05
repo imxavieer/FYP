e88c2a53a5dbab111180028dea178ff08730c376
liqipotato <89569278+liqipotato@users.noreply.github.com>
2023-02-05 15:58:48 +0800

    reservation ui functionalities

--- customer-frontend/src/pages/reserve.js
+++ customer-frontend/src/pages/reserve.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { styled } from '@mui/material/styles';
+import './reserve.css';
 
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -7,9 +8,16 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import Paper from '@mui/material/Paper';
 
 const BookTableButton = styled(Button)({
   padding: "12px 40px",
@@ -26,10 +34,25 @@ const BookTableButton = styled(Button)({
 
 function Reserve() {
   const [numpax, setNumPax] = React.useState('');
-
-  const handleChange = (event) => {
+  const handleNumPaxChange = (event) => {
     setNumPax(event.target.value);
   };
+
+  var [reserveDate, setDate] = React.useState();
+  const handleDateChange = (newValue) => {
+    setDate(newValue);
+    reserveDate = newValue.$d
+    reserveDate = JSON.stringify(reserveDate)
+  }
+  const todayDate = new Date();
+
+  const [timeslot, setTimeslot] = React.useState('');
+  const handleTimeslotChange = (event) => {
+    setTimeslot(event.target.value);
+  };
+
+  const [showConfirmationPaper, setShowPaper] = React.useState(false);
+  const openConfirmationPaper = () => setShowPaper(true);
 
   return (
     <React.Fragment>
@@ -51,7 +74,7 @@ function Reserve() {
                   id="num-pax"
                   value={numpax}
                   label="Number of People"
-                  onChange={handleChange}
+                  onChange={handleNumPaxChange}
                   >
                   <MenuItem value={1}>1 Person</MenuItem>
                   <MenuItem value={2}>2 Person</MenuItem>
@@ -68,42 +91,86 @@ function Reserve() {
           </Grid>
 
           <Grid item xs={3}>
-            <TextField
-              id="date"
-              label="Date"
-              type="date"
-              defaultValue="YYYY-MM-DD"
-              sx={{ width: 220 }}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Stack spacing={3}>
+                <DesktopDatePicker
+                  label="Date"
+                  value={reserveDate}
+                  inputFormat="DD/MM/YYYY"
+                  minDate={todayDate}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Stack>
+            </LocalizationProvider>
           </Grid>
 
           <Grid item xs={3}>
-            <TextField
-              id="time"
-              label="Time"
-              type="time"
-              defaultValue="12:00"
-              minutesStep="30"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              inputProps={{
-                step: 1800, // 30 min
-              }}
-              sx={{ width: 150 }}
-            />
+          <FormControl sx={{ minWidth: 300 }}>
+              <InputLabel id="timeslot-label">Time</InputLabel>
+                <Select
+                  labelId="timeslot-label"
+                  id="timeslot"
+                  value={timeslot}
+                  label="Time"
+                  onChange={handleTimeslotChange}
+                  >
+                  <MenuItem value={1130}>11:30 AM</MenuItem>
+                  <MenuItem value={1200}>12:00 PM</MenuItem>
+                  <MenuItem value={1230}>12:30 PM</MenuItem>
+                  <MenuItem value={1300}>1:00 PM</MenuItem>
+                  <MenuItem value={1330}>1:30 PM</MenuItem>
+                  <MenuItem value={1400}>2:00 PM</MenuItem>
+                  <MenuItem value={1430}>2:30 PM</MenuItem>
+                  <MenuItem value={1500}>3:00 PM</MenuItem>
+                  <MenuItem value={1530}>3:30 PM</MenuItem>
+                  <MenuItem value={1600}>4:00 PM</MenuItem>
+                  <MenuItem value={1630}>4:30 PM</MenuItem>
+                  <MenuItem value={1700}>5:00 PM</MenuItem>
+                  <MenuItem value={1730}>5:30 PM</MenuItem>
+                  <MenuItem value={1800}>6:00 PM</MenuItem>
+                  <MenuItem value={1830}>6:30 PM</MenuItem>
+                  <MenuItem value={1900}>7:00 PM</MenuItem>
+                  <MenuItem value={1930}>7:30 PM</MenuItem>
+                  <MenuItem value={2000}>8:00 PM</MenuItem>
+                  <MenuItem value={2030}>8:30 PM</MenuItem>
+                  <MenuItem value={2100}>9:00 PM</MenuItem>
+                  <MenuItem value={2130}>9:30 PM</MenuItem>
+                  <MenuItem value={2200}>10:00 PM</MenuItem>
+                </Select>
+            </FormControl>
           </Grid>
 
           <Grid item xs={3}>
-            <BookTableButton variant="contained">BOOK A TABLE</BookTableButton>
+            <BookTableButton variant="contained" onClick={openConfirmationPaper}>NEXT</BookTableButton>
           </Grid>
         </Grid>
-      </Box>
 
-      
+        {showConfirmationPaper && (
+          <Grid>
+            <Paper elevation={3} className='confirmation-paper'>
+              <h2>{JSON.stringify(reserveDate).substring(9,11)}/{JSON.stringify(reserveDate).substring(6,8)}/{JSON.stringify(reserveDate).substring(1,5)} - {timeslot}</h2>
+              <p>Number of People: {numpax}</p>
+              <b><p>Please confirm your reservation by entering your email address.</p></b>
+              <b><p>You will receive a confirmation email upon booking.</p></b>
+              <Grid container>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
+                  <TextField
+                    required
+                    id="standard-required"
+                    label="Email Address"
+                    defaultValue=""
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <BookTableButton variant="contained">BOOK A TABLE</BookTableButton>
+                </Grid>
+              </Grid>
+            </Paper>
+        </Grid>)}
+      </Box>
     </React.Fragment>
   );
 }
