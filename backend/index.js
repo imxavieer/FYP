30a92b8085fbde7198187b98b1b6b23a56879bd0
Setsuna Yukiii <siang-meng@live.com>
2023-02-08 00:08:17 +0800

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,26 @@
+// ==================imports==================
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const cors = require("cors");
-const app = express();
-app.use(cors());
+require("dotenv").config();
 
+// ==================configure app settings==================
+app.use(cors());
 app.use(bodyParser.json());
 
 const ReservationRoute = require("./Routes/ReservationRoutes");
 
-app.use("/api/reservation", ReservationRoute);
 
+// ==================use routes==================
+app.use("/api/reservation", ReservationRoute);
 app.use((error, req, res, next) => {
     res.status(error.code || 500);
     res.json({ message: error.message || "An unknown error occurred" });
 });
 
+// ==================initialise==================
 mongoose
     .connect(
         "mongodb+srv://sanghil:1058022a@cluster0.ahpmxxy.mongodb.net/FYP?retryWrites=true&w=majority"
