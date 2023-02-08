b836339776159074e0e29d00e49b5a9f4117c3d9
Setsuna Yukiii <siang-meng@live.com>
2023-02-08 13:06:41 +0800

    retrieve reservation by ID (frontend)

--- admin-frontend/src/pages/reservations/cancelreservation/cancelreservation.js
+++ admin-frontend/src/pages/reservations/cancelreservation/cancelreservation.js
@@ -3,39 +3,62 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 function CancelReservation() {
     const [loading, setLoading] = useState(true);
-    const [success, setSuccess] = useState(false);
+    // 2 successes --> loading & deleting
+    // load success --> to retrive reservation
+    const [loadSuccess, setLoadSuccess] = useState(false);
+    // delete success --> to delete reservation
+    const [deleteSuccess, setDeleteSuccess] = useState(false);
     const { reservationId } = useParams();
+    const [reservation, setReservation] = useState(null);
+
+    // 2 key requests
+    // find reservation
+    const getReservationById = async () => {
+        await axios
+            .get(
+                `${process.env.REACT_APP_BACKEND_URL}reservation/${reservationId}`
+            )
+            .then((result) => {
+                setReservation(result);
+                setLoading(false);
+                setLoadSuccess(true);
+            })
+            .catch((err) => {
+                setLoading(false);
+                setLoadSuccess(false);
+            });
+    };
+    // delete reservation (trigggerd by confirmation)
     const cancelReservation = async () => {
         await axios
             .delete(
                 `${process.env.REACT_APP_BACKEND_URL}reservation/${reservationId}`
             )
             .then(() => {
-                setLoading(false);
-                setSuccess(true);
+                setDeleteSuccess(true);
             })
             .catch(() => {
-                setLoading(false);
+                setDeleteSuccess(false)
             });
     };
     useEffect(() => {
-        cancelReservation();
+        getReservationById();
     }, []);
     return (
         <div>
             {loading ? (
                 <>
-                    <h1>Cancelling</h1>
+                    <h1>Loading ...</h1>
                 </>
             ) : (
                 <>
-                    {success ? (
+                    {loadSuccess ? (
                         <>
-                            <h1>Cancelled successfully!</h1>
+                            <h1>Reservation found</h1>
                         </>
                     ) : (
                         <>
-                            <h1>Cancellation failed!</h1>
+                            <h1>Reservation Not found</h1>
                         </>
                     )}
                 </>
