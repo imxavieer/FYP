26e212536cec98a14633a688f5d7aadcaf185399
Setsuna Yukiii <siang-meng@live.com>
2023-02-10 17:17:05 +0800

    "order me" prompt animation

--- customer-frontend/src/components/PopupPrompt.js
+++ customer-frontend/src/components/PopupPrompt.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import "./PopupPrompt.css";
 import "../index.css";
 import DiningIcon from "@mui/icons-material/Dining";
 import { useLocation } from "react-router-dom";
 function PopupPrompt() {
-  const { pathname } = useLocation();
-  const redirectToOrder = () => {
-    window.location.href = "https://qashiereats.com/honeynight";
-  };
-  if (pathname === "/reserve") {
-    return <></>;
-  } else {
-    return (
-      <Box
-        className="popupContainer"
-        onClick={() => {
-          window.open("https://qashiereats.com/honeynight");
-        }}
-      >
-        <Box className="iconContainer">
-          <DiningIcon />
-        </Box>
-        Order here
-      </Box>
-    );
-  }
+    const { pathname } = useLocation();
+    const [hovered, setHovered] = useState(false);
+    const redirectToOrder = () => {
+        window.location.href = "https://qashiereats.com/honeynight";
+    };
+    if (pathname === "/reserve") {
+        return <></>;
+    } else {
+        return (
+            <Box
+                className="popupContainer"
+                onClick={() => {
+                    window.open("https://qashiereats.com/honeynight");
+                }}
+                onMouseEnter={() => {
+                    setHovered(true);
+                }}
+                onMouseLeave={() => {
+                    setHovered(false);
+                }}
+            >
+                {!hovered ? (
+                    <>
+                        <>Order here</>
+                        <hr />
+                    </>
+                ) : (
+                    <></>
+                )}
+
+                <Box className="iconContainer">
+                    <DiningIcon />
+                </Box>
+                {hovered ? <>Click to order!</> : <></>}
+            </Box>
+        );
+    }
 }
 
 export default PopupPrompt;
