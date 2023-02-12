29c4a619267fd3101f1d69c8a7710dee9e398292
lokim0215 <92701955+lokim0215@users.noreply.github.com>
2023-02-12 13:01:04 +0800

    changed one of carousel fotos

--- customer-frontend/src/components/landingpage/homesection/home.js
+++ customer-frontend/src/components/landingpage/homesection/home.js
@@ -6,93 +6,93 @@ import { Button } from "@mui/material";
 import "./home.css";
 
 function Home() {
-    // THIS CODE IS IMPORTANT FROM HERE
-    let location = useLocation();
-    const pictures = [
-        "https://i.imgur.com/3hoca6H.jpg",
-        "https://insanelygoodrecipes.com/wp-content/uploads/2020/12/Korean-Side-Dishes.png",
-        "https://media.timeout.com/images/105303587/image.jpg",
-    ];
-    const [currentPicture, setCurrentPicture] = useState(0);
-    useEffect(() => {
-        if (location.hash) {
-            let elem = document.getElementById(location.hash.slice(1));
-            if (elem) {
-                elem.scrollIntoView({ behavior: "smooth" });
-            }
-        } else {
-            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-        }
-    }, [location]);
-
-    const changePictureOnRight = () => {
-        if (currentPicture == pictures.length - 1) {
-            changePicture(0);
-        } else {
-            changePicture(currentPicture + 1);
-        }
-    };
-
-    const changePictureOnLeft = () => {
-        if (currentPicture == 0) {
-            changePicture(pictures.length - 1);
-        } else {
-            changePicture(currentPicture - 1);
-        }
-    };
-
-    // to add animation
-    const [opacity, setOpacity] = useState(1);
-    const changePicture = (newPicture) => {
-        setOpacity(0);
-        const timer = setTimeout(() => {
-            setCurrentPicture(newPicture);
-            setOpacity(1);
-        }, 350);
-        return () => clearTimeout(timer);
-    };
-
-    const { pathname } = useLocation();
-    if (pathname === "/reserve") {
-        return <></>;
+  // THIS CODE IS IMPORTANT FROM HERE
+  let location = useLocation();
+  const pictures = [
+    "https://i.imgur.com/3hoca6H.jpg",
+    "https://i.imgur.com/2gfJw9o.png",
+    "https://insanelygoodrecipes.com/wp-content/uploads/2020/12/Korean-Side-Dishes.png",
+  ];
+  const [currentPicture, setCurrentPicture] = useState(0);
+  useEffect(() => {
+    if (location.hash) {
+      let elem = document.getElementById(location.hash.slice(1));
+      if (elem) {
+        elem.scrollIntoView({ behavior: "smooth" });
+      }
     } else {
-        return (
-            <>
-                {/* // THIS CODE IS IMPORTANT UNTIL HERE  */}
-                <div className="home-div" id="home-div">
-                    <img
-                        src={pictures[currentPicture]}
-                        alt="honeyNightfood"
-                        className="home-background"
-                        style={{
-                            opacity: opacity,
-                        }}
-                    />
-                    <div className="home-centered-text">
-                        Delivering the most authentic Korean taste
-                    </div>
-                    <div className="arrowButtonContainer">
-                        <Button
-                            sx={{ color: "white" }}
-                            onClick={() => {
-                                changePictureOnLeft();
-                            }}
-                        >
-                            <ArrowBackIosIcon />
-                        </Button>
-                        <Button
-                            sx={{ color: "white" }}
-                            onClick={() => {
-                                changePictureOnRight();
-                            }}
-                        >
-                            <ArrowForwardIosIcon />
-                        </Button>
-                    </div>
-                </div>
-            </>
-        );
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     }
+  }, [location]);
+
+  const changePictureOnRight = () => {
+    if (currentPicture == pictures.length - 1) {
+      changePicture(0);
+    } else {
+      changePicture(currentPicture + 1);
+    }
+  };
+
+  const changePictureOnLeft = () => {
+    if (currentPicture == 0) {
+      changePicture(pictures.length - 1);
+    } else {
+      changePicture(currentPicture - 1);
+    }
+  };
+
+  // to add animation
+  const [opacity, setOpacity] = useState(1);
+  const changePicture = (newPicture) => {
+    setOpacity(0);
+    const timer = setTimeout(() => {
+      setCurrentPicture(newPicture);
+      setOpacity(1);
+    }, 350);
+    return () => clearTimeout(timer);
+  };
+
+  const { pathname } = useLocation();
+  if (pathname === "/reserve") {
+    return <></>;
+  } else {
+    return (
+      <>
+        {/* // THIS CODE IS IMPORTANT UNTIL HERE  */}
+        <div className="home-div" id="home-div">
+          <img
+            src={pictures[currentPicture]}
+            alt="honeyNightfood"
+            className="home-background"
+            style={{
+              opacity: opacity,
+            }}
+          />
+          <div className="home-centered-text">
+            Delivering the most authentic Korean taste
+          </div>
+          <div className="arrowButtonContainer">
+            <Button
+              sx={{ color: "white" }}
+              onClick={() => {
+                changePictureOnLeft();
+              }}
+            >
+              <ArrowBackIosIcon />
+            </Button>
+            <Button
+              sx={{ color: "white" }}
+              onClick={() => {
+                changePictureOnRight();
+              }}
+            >
+              <ArrowForwardIosIcon />
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Home;
